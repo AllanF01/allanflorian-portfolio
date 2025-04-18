840bfc18d36bd73237c1259766a78d9691d3b399
AllanF01 <allanflorian8@gmail.com>
2025-04-18 11:13:13 -0400

    improved styles

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function toggleDarkMode() {
-    document.body.classList.toggle('dark');
+    const body = document.body;
+    const icon = document.getElementById("theme-icon");
+  
+    body.classList.toggle("dark");
+  
+    if (body.classList.contains("dark")) {
+      icon.classList.remove("fa-sun");
+      icon.classList.add("fa-moon");
+    } else {
+      icon.classList.remove("fa-moon");
+      icon.classList.add("fa-sun");
+    }
   }
   
